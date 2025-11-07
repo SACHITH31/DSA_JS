a25b7ffd60caf4fff762b19ab925794029155eb5
KASA <kasasachith@gmail.com>
2025-11-07 20:21:39 +0530

    added second type of printing even numbers

--- 1LOOPS/2printEvenNumbers.js
+++ 1LOOPS/2printEvenNumbers.js
@@ -1,6 +1,14 @@
 // First way of printing even numbers using the "%" operator
 for (let i = 0; i <= 10; i++) {
     if (i % 2 == 0) {
+        // console.log(`${i} is even number.`)
+    }
+}
+
+// Second way of printing even numbers using bitwise &(AND) operators
+
+for (let i = 0; i <= 10; i++) {
+    if ((i & 2) === 0) {
         console.log(`${i} is even number.`)
     }
 }
