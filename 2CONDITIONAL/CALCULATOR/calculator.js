00c083785b89cb37102b87ea065646ee4deda88e
KASA <kasasachith@gmail.com>
2025-11-19 17:03:01 +0530

    completed all clear functionality

--- 2CONDITIONAL/CALCULATOR/calculator.js
+++ 2CONDITIONAL/CALCULATOR/calculator.js
@@ -1,10 +1,25 @@
-const buttons = document.querySelectorAll('.button');
-const result = document.querySelector('.result')
+let buttons = document.querySelectorAll('.button');
+let result = document.querySelector('.result')
+let enteredValue;
 
 for (let button of buttons) {
     button.addEventListener('click', (e) => {
         if (button.innerText !== '=') {
-            result.value += button.innerText;
+            if (button.innerText !== "AC") {
+                //while clicking on 1, 2, all except =, Ac
+                result.value += button.innerText;
+                enteredValue = result.value;
+            } else {
+                //while clicked on AC
+                result.value = ' '
+                console.log(`Clicked on: ${button.innerText}`)
+                console.log(`Entered Value is: ${enteredValue}`)
+            }
+        } else if (button.innerText === '='){
+            //while clicked on =
+            console.log(`Clicked on: ${button.innerText}`)
+            console.log(`Entered Value is: ${enteredValue}`)
         }
     })
 }
+
