298382518071b6a14ba3d1d1208802b1c31ff008
KASA <kasasachith@gmail.com>
2025-11-19 16:24:01 +0530

    user can enter the numbers into the textarea

--- 2CONDITIONAL/CALCULATOR/calculator.js
+++ 2CONDITIONAL/CALCULATOR/calculator.js
@@ -1,1 +1,10 @@
+const buttons = document.querySelectorAll('.button');
+const result = document.querySelector('.result')
 
+for (let button of buttons) {
+    button.addEventListener('click', (e) => {
+        if (button.innerText !== '=') {
+            result.value += button.innerText;
+        }
+    })
+}
