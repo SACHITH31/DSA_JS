e2abb81ff371466199ee4c16bb63026e258af948
KASA <kasasachith@gmail.com>
2025-11-19 17:23:01 +0530

    implemented the X feature

--- 2CONDITIONAL/CALCULATOR/calculator.js
+++ 2CONDITIONAL/CALCULATOR/calculator.js
@@ -1,25 +1,24 @@
 let buttons = document.querySelectorAll('.button');
 let result = document.querySelector('.result')
-let enteredValue;
+// let enteredValue;
 
 for (let button of buttons) {
     button.addEventListener('click', (e) => {
-        if (button.innerText !== '=') {
-            if (button.innerText !== "AC") {
-                //while clicking on 1, 2, all except =, Ac
-                result.value += button.innerText;
-                enteredValue = result.value;
-            } else {
-                //while clicked on AC
-                result.value = ' '
-                console.log(`Clicked on: ${button.innerText}`)
-                console.log(`Entered Value is: ${enteredValue}`)
-            }
-        } else if (button.innerText === '='){
-            //while clicked on =
-            console.log(`Clicked on: ${button.innerText}`)
-            console.log(`Entered Value is: ${enteredValue}`)
-        }
+      if (button.innerText === 'AC') {
+        result.value = ' '
+        console.log("Clicked on Ac")  
+    } else if (button.innerText === 'X') {
+        let temp = result.value
+        temp = temp.slice(0, -1)
+        result.value = temp
+        console.log("Clicked on X")  
+    } else if (button.innerText === '=') {
+        result.value = ' '
+        console.log("Cliced on =")
+    } else {
+        result.value += button.innerText
+        // console.log(typeof (result.value += button.innerText))
+    }
     })
 }
 
