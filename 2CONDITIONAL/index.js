c553867be67367a66aa083c1c0072846cb1e0a7b
KASA <kasasachith@gmail.com>
2025-11-19 14:46:54 +0530

    completed even or odd

--- 2CONDITIONAL/index.js
+++ 2CONDITIONAL/index.js
@@ -12,23 +12,30 @@
 
 
 //largest of 3 numbers
-const num1 = 1222
-const num2 = 20
-const num3 = 1000000000
+// const num1 = 1222
+// const num2 = 20
+// const num3 = 1000000000
+// let big = num1
+// if (big < num2) {
+//     if (big < num3) {
+//         big = num3
+//     } else {
+//         big = num2
+//     }
+// } else {
+//     if (big < num3) {
+//         big = num3
+//     }
+// }
+// console.log(big);
 
-let big = num1
-
-if (big < num2) {
-    if (big < num3) {
-        big = num3
-    } else {
-        big = num2
-    }
-} else {
-    if (big < num3) {
-        big = num3
-    }
-}
-
-console.log(big);
-
+//even or odd 
+  //working of &: if both are 1 then returns 1 or else 0
+// const myNum = 10
+// if ((myNum & 1) === 0) {
+//     console.log(`${myNum} is even number.`)
+//     // console.log((myNum & 1))
+// } else {
+//     console.log(`${myNum} is odd number.`)
+//     // console.log((myNum & 1))
+// }
