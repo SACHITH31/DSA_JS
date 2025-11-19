4e3ae9e9457d07c8196853c1534c7d70cf77818f
KASA <kasasachith@gmail.com>
2025-11-19 10:11:46 +0530

    learnt about the prority of BODMAS

--- SHERIYANS_DSA/yWeird.js
+++ SHERIYANS_DSA/yWeird.js
@@ -1,8 +1,16 @@
 // WEIRD than any LANGUAGE😪
+
 a = 10
 // console.log(a);
 var a 
 
 let b = '12';
+//concatenation
 console.log(a + b) // 1012 (in console the colour is BLUE for NUMBERS)
 console.log(typeof(a + b)) //string (in console the colour is BLACK for STRINGS)
+
+let num1 = 10
+let num2 = 20
+console.log(num1 + num2); // 30
+console.log("The sum is: " + num1 + num2);  // 1020 bcz string + num + num, so first string + num(concatenation) = value(type is string), then this value(string) + remaining = string only means(1020)
+console.log("The sum is: " + (num1 + num2)); // 30 bcz of BODMAS
