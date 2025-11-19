441ba682e70c73be3d2b9a1937cd14543e31b8d4
KASA <kasasachith@gmail.com>
2025-11-19 14:21:41 +0530

    completed +ve, -ve, 0 problem

---
+++ 2CONDITIONAL/index.js
@@ -0,0 +1,11 @@
+//+ve, -ve, 0
+const myNum = 10
+if (myNum > 0) {
+    console.log(`${myNum} is positive number`)
+} else if (myNum < 0) {
+    console.log(`${myNum} is negative number`)
+} else if (myNum === 0) {
+    console.log(`${myNum} is zero`)
+} else {
+    console.log("Check the type😏")
+}
