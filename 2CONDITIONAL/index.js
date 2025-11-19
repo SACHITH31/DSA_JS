9afd643bb0e9c5a59c8c4e2aca94419e342000e4
KASA <kasasachith@gmail.com>
2025-11-19 18:00:53 +0530

    completed leap year program

--- 2CONDITIONAL/index.js
+++ 2CONDITIONAL/index.js
@@ -95,3 +95,12 @@
 // }
 // // console.log(myObj)
 
+//leap year
+let year = 2024;
+
+if ((year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0)) {
+  console.log(year + " is a Leap Year");
+} else {
+  console.log(year + " is NOT a Leap Year");
+}
+
