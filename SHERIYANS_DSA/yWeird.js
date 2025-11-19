1986fe6778439653129631ca94c0c1b4540fc41f
KASA <kasasachith@gmail.com>
2025-11-19 10:22:50 +0530

    learnt about the COERCION

--- SHERIYANS_DSA/yWeird.js
+++ SHERIYANS_DSA/yWeird.js
@@ -14,3 +14,13 @@ let num2 = 20
 console.log(num1 + num2); // 30
 console.log("The sum is: " + num1 + num2);  // 1020 bcz string + num + num, so first string + num(concatenation) = value(type is string), then this value(string) + remaining = string only means(1020)
 console.log("The sum is: " + (num1 + num2)); // 30 bcz of BODMAS
+console.log(num1 + num2 + "is the sum of 10 and 20"); // 30
+
+
+//ANOTHER BEAUTY OF JS🙂😪
+console.log('1' + 1) //11
+console.log('1' - 1) //0
+console.log('1' * 1) //0
+//Type coercion in JavaScript refers to the process of converting a value from one data type to another. There are 2 types they are: 
+//  1. Implicit Coercion: done by JS ENGINE ONLY.
+//  2. Explicit Coercion: done by MANUALLY.
