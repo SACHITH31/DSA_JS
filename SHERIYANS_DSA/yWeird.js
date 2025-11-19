84c5b406dd20af23d0910d2d9e168879f3889eb0
KASA <kasasachith@gmail.com>
2025-11-19 10:02:27 +0530

    about number and string

--- SHERIYANS_DSA/yWeird.js
+++ SHERIYANS_DSA/yWeird.js
@@ -3,3 +3,6 @@ a = 10
 // console.log(a);
 var a 
 
+let b = '12';
+console.log(a + b) // 1012 (in console the colour is BLUE for NUMBERS)
+console.log(typeof(a + b)) //string (in console the colour is BLACK for STRINGS)
