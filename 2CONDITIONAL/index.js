58d9577dc6bc87f21eb1972d92f9bd74364271a8
KASA <kasasachith@gmail.com>
2025-11-19 14:27:43 +0530

    completed largest of 3 numbers program

--- 2CONDITIONAL/index.js
+++ 2CONDITIONAL/index.js
@@ -1,11 +1,34 @@
 //+ve, -ve, 0
-const myNum = 10
-if (myNum > 0) {
-    console.log(`${myNum} is positive number`)
-} else if (myNum < 0) {
-    console.log(`${myNum} is negative number`)
-} else if (myNum === 0) {
-    console.log(`${myNum} is zero`)
+// const myNum = 10
+// if (myNum > 0) {
+//     console.log(`${myNum} is positive number`)
+// } else if (myNum < 0) {
+//     console.log(`${myNum} is negative number`)
+// } else if (myNum === 0) {
+//     console.log(`${myNum} is zero`)
+// } else {
+//     console.log("Check the type😏")
+// }
+
+
+//largest of 3 numbers
+const num1 = 1222
+const num2 = 20
+const num3 = 1000000000
+
+let big = num1
+
+if (big < num2) {
+    if (big < num3) {
+        big = num3
+    } else {
+        big = num2
+    }
 } else {
-    console.log("Check the type😏")
+    if (big < num3) {
+        big = num3
+    }
 }
+
+console.log(big);
+
