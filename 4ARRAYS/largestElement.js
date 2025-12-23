a5abdfebd42e107a75ba17794a30eae20f0c4a0d
SACHITH <kasasachith@gmail.com>
2025-12-23 20:05:59 +0530

    returns the result

--- 4ARRAYS/largestElement.js
+++ 4ARRAYS/largestElement.js
@@ -1,17 +1,17 @@
 // finding the largest element in the given array 
 
-function findLargestElement (arr) {
-    const myArr = arr
+function findLargestElement (myArr) {
     let big = myArr[0]
-
+    
     //loop to find the largest element and store it in big 
     for (let i = 0; i < myArr.length; i++) {
         if (big < myArr[i]) {
             big = myArr[i]
         }
     }
-    console.log(`The largest element is: ${big}`)
+    return (`The largest element is: ${big}`)
 }
 
 const myArr = [3, 7, 1, 9, 4]
-findLargestElement(myArr)
+const result = findLargestElement(myArr)
+console.log(result);
