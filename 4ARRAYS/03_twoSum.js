41b3ace1bbbd3c468e2a967acf87e90fb5d16d1b
SACHITH <kasasachith@gmail.com>
2025-12-23 21:17:33 +0530

    completed the twoSum problem

--- 4ARRAYS/03_twoSum.js
+++ 4ARRAYS/03_twoSum.js
@@ -3,13 +3,15 @@
 function twoSum (myArr, target) {
     const result = []
     for (let i = 0; i < myArr.length; i++) {
-        if (myArr[i] + myArr[i + 1] === target) {
-            result.push(i, i + 1)
+        for (let j = i + 1; j <= myArr.length; j++) {
+            if (myArr[i] + myArr[j] === target) {
+                result.push([i, j])
+            }
         }
     }
     console.log(result)
 }
 
-const myArr = [3, 2, 4]
-const target = 6
+const myArr = [5, 1, 3, 7]
+const target = 8
 twoSum(myArr, target)
