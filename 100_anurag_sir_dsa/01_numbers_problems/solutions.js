71159cff39e24a2f527742c5f791e954823d249b
SACHITH <kasasachith@gmail.com>
2026-01-01 14:16:29 +0530

    shows the sum from 1 to user entered number

---
+++ 100_anurag_sir_dsa/01_numbers_problems/solutions.js
@@ -0,0 +1,37 @@
+const datePicker = document.querySelector('.numberPicker')
+const submitButton = document.querySelector('.submitButton')
+const showResults = document.querySelector('.showResults')
+
+
+submitButton.addEventListener ('click', () => {
+  const temp = datePicker.value
+  for (let i = temp; i >= 1; i--) {
+    const li = document.createElement('li')
+    li.innerText = i
+    showResults.append(li)
+  }
+})
+
+const evenSubmitButton = document.querySelector('.evenSubmitButton')
+evenSubmitButton.addEventListener('click', () => {
+    const temp = datePicker.value
+    for (let i = 1; i <= temp; i++) {
+        if (i % 2 == 0) {
+            const li = document.createElement('li')
+            li.innerText = i
+            showResults.append(li)
+        }
+    }
+})
+
+const showSumButton = document.querySelector('.showSumButton')
+showSumButton.addEventListener('click', () => {
+    const temp = datePicker.value
+    const p = document.createElement('p')
+    let sum = 0
+    for (let i = 1; i <= temp; i++) {
+        sum = sum + i
+    }
+    p.innerText = `The sum of 1 to ${temp} is: ${sum}`
+    showResults.append(p) 
+})
