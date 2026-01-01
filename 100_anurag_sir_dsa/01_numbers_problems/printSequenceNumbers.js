9a1822b790b52cb6375847696b16826031b07649
SACHITH <kasasachith@gmail.com>
2026-01-01 14:05:51 +0530

    prints in the reverse order

--- 100_anurag_sir_dsa/01_numbers_problems/printSequenceNumbers.js
+++ 100_anurag_sir_dsa/01_numbers_problems/printSequenceNumbers.js
@@ -7,37 +7,39 @@ const datePicker = document.querySelector('.numberPicker')
 const submitButton = document.querySelector('.submitButton')
 const showResults = document.querySelector('.showResults')
 
+
 submitButton.addEventListener ('click', () => {
-  for (let i = 1; i <= datePicker.value; i++) {
+  const temp = datePicker.value
+  for (let i = temp; i >= 1; i--) {
     const li = document.createElement('li')
     li.innerText = i
     showResults.append(li)
   }
 })
 
-const num = 2;
+// const num = 2;
 
-{
-  console.log("Using for loop");
-  for (let i = 1; i <= num; i++) {
-    console.log(i);
-  }
-}
+// {
+//   console.log("Using for loop");
+//   for (let i = 1; i <= num; i++) {
+//     console.log(i);
+//   }
+// }
 
-{
-  console.log("Using while loop");
-  let i = 1;
-  while (i <= num) {
-    console.log(i);
-    i++;
-  }
-}
+// {
+//   console.log("Using while loop");
+//   let i = 1;
+//   while (i <= num) {
+//     console.log(i);
+//     i++;
+//   }
+// }
 
-{
-  console.log("Using do-while loop");
-  let i = 1;
-  do {
-    console.log(i);
-    i++;
-  } while (i <= num);
-}
+// {
+//   console.log("Using do-while loop");
+//   let i = 1;
+//   do {
+//     console.log(i);
+//     i++;
+//   } while (i <= num);
+// }
