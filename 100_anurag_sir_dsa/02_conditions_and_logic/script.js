18a840c10b539258eae85b7c98f5296b6230831a
SACHITH <kasasachith@gmail.com>
2026-01-02 20:21:15 +0530

    completed the calculating electricity bill

--- 100_anurag_sir_dsa/02_conditions_and_logic/script.js
+++ 100_anurag_sir_dsa/02_conditions_and_logic/script.js
@@ -1,5 +1,4 @@
 // find the largest of 3 numbers
-
 function largestOf3Numbers (num1, num2, num3) {
     let big = num1
     if (num1 > num2) {
@@ -18,10 +17,9 @@ function largestOf3Numbers (num1, num2, num3) {
 
 const result1 = largestOf3Numbers(20, 20, 1)
 console.log(result1);
- 
+
+
 // find wheather the number is postive, negative or zero
-
-
 function checkingTheNumber (num) {
     if (typeof num === 'number') {
         if (num === 0) {
@@ -35,7 +33,30 @@ function checkingTheNumber (num) {
         return `Please Enter valid Number.`
     }
 }
-
 const myNum = 5
 const result2 = checkingTheNumber(myNum)
 console.log(result2);
+
+
+// calculate electricity bill
+function findCurrentBill (units) {
+    let sum = 0
+    for (let i = 1; i <= units; i++) {
+        if (i <= 100) {
+            sum += 5
+        } else if (i > 100 && i <= 200) {
+            sum += 7
+        } else if (i > 200 && i <= 300) {
+            sum += 10
+        } else {
+            sum += 12
+        }
+    };
+    return sum
+}
+const units = 230
+const result3 = findCurrentBill(units)
+console.log(result3);
+
+
+
