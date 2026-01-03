44d709af36b996c6f7f48dfd67bb6d43a8d43396
SACHITH <kasasachith@gmail.com>
2026-01-03 19:35:38 +0530

    completed pyramid pattern

--- 100_anurag_sir_dsa/03_star_patterns/script.js
+++ 100_anurag_sir_dsa/03_star_patterns/script.js
@@ -16,6 +16,21 @@ const rows = 5;
         for (let j = 0; j < i; j++) {
             temp += '*'
         }
-        console.log(temp)
+        // console.log(temp)
+    }
+}
+
+
+// pyramid pattern
+{
+    for (let i = 1; i <= rows; i++) {
+        let temp = ''
+        for (let j = 1; j <= rows - i; j++) {
+            temp += ' '
+        }
+        for (let k = 1; k <= 2 * i - 1; k++) {
+            temp += '*'
+        }
+        // console.log(temp)
     }
 }
